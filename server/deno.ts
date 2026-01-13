24db47da317c877a7f09691545e4af530adc9d96
tcely <tcely@users.noreply.github.com>
2026-01-13 13:35:15 -0500

    Change require to import for proxy operation

--- server/deno.ts
+++ server/deno.ts
@@ -7,7 +7,7 @@ const PORT = parseInt(Deno.env.get("PORT") || "8000");
 const operation = Deno.env.get("WHICH") || "server";
 
 if ( "proxy" === operation ) {
-    require(join(import.meta.dirname, "..", "proxy", "deno.ts"));
+    import join(import.meta.dirname, "..", "proxy", "deno.ts");
 } else {
     Deno.serve(
         {
