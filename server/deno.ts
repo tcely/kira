be62f49212b1fc1a67534b1067ca9c02fe6d319c
tcely <tcely@users.noreply.github.com>
2026-01-13 12:43:17 -0500

    dist should be one level up

--- server/deno.ts
+++ server/deno.ts
@@ -12,7 +12,7 @@ Deno.serve(
     (req: Request) => {
         // Serve the current directory by default, or specify a different one with the 'fsRoot' option.
         return serveDir(req, {
-            fsRoot: join(import.meta.dirname, "dist"), // Serves files from the 'dist' directory
+            fsRoot: join(import.meta.dirname, "..", "dist"), // Serves files from the 'dist' directory
             showDirListing: false, // Optional: enables directory listings in the browser
         });
     },
