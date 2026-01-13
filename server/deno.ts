711cab5a78a201c4cfcaa614d15752b68854e5b0
tcely <tcely@users.noreply.github.com>
2026-01-13 12:29:27 -0500

    Try to serve the bundled files

--- server/deno.ts
+++ server/deno.ts
@@ -1,4 +1,5 @@
 import { serveDir } from "jsr:@std/http/file-server";
+import { join } from "jsr:@std/path/posix/join";
 
 const HOST = Deno.env.get("HOST") || "0.0.0.0";
 const PORT = parseInt(Deno.env.get("PORT") || "8000");
@@ -11,7 +12,7 @@ Deno.serve(
     (req: Request) => {
         // Serve the current directory by default, or specify a different one with the 'fsRoot' option.
         return serveDir(req, {
-            fsRoot: "/dist", // Serves files from the 'dist' directory
+            fsRoot: join(import.meta.dirname, "dist"), // Serves files from the 'dist' directory
             showDirListing: false, // Optional: enables directory listings in the browser
         });
     },
