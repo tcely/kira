7ac2cd9f7d6d7e007e606faeb2d48547b9305f1a
tcely <tcely@users.noreply.github.com>
2026-01-13 13:00:09 -0500

    Try multiple operations

--- server/deno.ts
+++ server/deno.ts
@@ -4,16 +4,22 @@ import { join } from "jsr:@std/path/posix/join";
 const HOST = Deno.env.get("HOST") || "0.0.0.0";
 const PORT = parseInt(Deno.env.get("PORT") || "8000");
 
-Deno.serve(
-    {
-        hostname: HOST,
-        port: PORT,
-    },
-    (req: Request) => {
-        // Serve the current directory by default, or specify a different one with the 'fsRoot' option.
-        return serveDir(req, {
-            fsRoot: join(import.meta.dirname, "..", "dist"), // Serves files from the 'dist' directory
-            showDirListing: false, // Optional: enables directory listings in the browser
-        });
-    },
-);
+const operation = Deno.env.get("WHICH") || "server";
+
+if ( "proxy" === operation ) {
+    require(join(import.meta.dirname, "..", "proxy", "deno.ts"));
+} else {
+    Deno.serve(
+        {
+            hostname: HOST,
+            port: PORT,
+        },
+        (req: Request) => {
+            // Serve the current directory by default, or specify a different one with the 'fsRoot' option.
+            return serveDir(req, {
+                fsRoot: join(import.meta.dirname, "..", "dist"), // Serves files from the 'dist' directory
+                showDirListing: false, // Optional: enables directory listings in the browser
+            });
+        },
+    );
+}
