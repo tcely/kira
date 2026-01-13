1a80b2386c5906a5466e7f01614be3c7976c4cf6
tcely <tcely@users.noreply.github.com>
2026-01-13 09:55:39 -0500

    Configure server to use environment variables

--- server/deno.ts
+++ server/deno.ts
@@ -1,9 +1,18 @@
 import { serveDir } from "jsr:@std/http/file-server";
 
-Deno.serve((req: Request) => {
-    // Serve the current directory by default, or specify a different one with the 'fsRoot' option.
-    return serveDir(req, {
-        fsRoot: "../dist", // Serves files from the 'dist' directory
-        showDirListing: false, // Optional: enables directory listings in the browser
-    });
-});
+const HOST = Deno.env.get("HOST") || "0.0.0.0";
+const PORT = parseInt(Deno.env.get("PORT") || "8000");
+
+Deno.serve(
+    {
+        hostname: HOST,
+        port: PORT,
+    },
+    (req: Request) => {
+        // Serve the current directory by default, or specify a different one with the 'fsRoot' option.
+        return serveDir(req, {
+            fsRoot: "dist", // Serves files from the 'dist' directory
+            showDirListing: false, // Optional: enables directory listings in the browser
+        });
+    },
+);
